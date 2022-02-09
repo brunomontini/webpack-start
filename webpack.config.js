ad85fdfa7f41cff383db217b2a6a2035a9f97c55
Bruno Montini <brunomontini96@gmail.com>
2022-02-09 19:05:42 -0400

    Webpack start code

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path")
+
+module.exports ={
+    mode: 'development',
+
+    entry: {
+        index: './src/index.js'
+    },
+    output: {
+        filename:'[name].bundle.js',
+        path : path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, 
+                use :'babel-loader'
+            }
+            
+        ]
+    },
+
+    watch:true,
+
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"),
+            watch: true,
+        }
+    }
+}
